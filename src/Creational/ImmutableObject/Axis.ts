2b13dda774ff829581c8bed698d4987d14af8515
hadi Tajallaei <hadi.tajallaei@applearn.tv>
2018-08-28 15:28:20 +0100

    Added new pattern ImmutableObject

---
+++ src/Creational/ImmutableObject/Axis.ts
@@ -0,0 +1,11 @@
+export class Axis {
+  public constructor(private x: number, private y: number) {}
+
+  public getX(): number {
+    return this.x;
+  }
+
+  public getY(): number {
+    return this.y;
+  }
+}
