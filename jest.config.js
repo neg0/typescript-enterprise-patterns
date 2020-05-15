cd5bd515b189177d9ba504bd3c88fbfb49372aeb
Hadi Tajallaei <tajallaei@gmail.com>
2020-05-15 01:57:00 +0100

    finalised eslint installation, avoided plugin to connect prettier to eslint to avoid possible pipline issue and semantic versioning

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    moduleFileExtensions: ['js', 'd.ts', 'ts', 'tsx'],
-    "collectCoverage": true,
-    "coverageReporters": ["json", "html"],
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'd.ts', 'ts', 'tsx'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
 }
--- src/ControlFlow/Promises/Todos.ts
+++ src/ControlFlow/Promises/Todos.ts
@@ -1,67 +1,64 @@
 import { get } from 'https'
 
 const HttpResponse = (statusCode: number, headers: any, data: string) => {
-    return {
-        statusCode: statusCode,
-        headers: headers,
-        data: data,
-    }
+  return {
+    statusCode: statusCode,
+    headers: headers,
+    data: data,
+  }
 }
 
 export default function httpsGet(
-    url: string,
+  url: string,
 ): Promise<{ statusCode: number; headers: any; data: string }> {
-    return new Promise((resolve, reject) => {
-        get(url, res => {
-            const { headers } = res
-            const { statusCode } = res
-            let data: Uint8Array[] = []
+  return new Promise((resolve, reject) => {
+    get(url, res => {
+      const { headers } = res
+      const { statusCode } = res
+      let data: Uint8Array[] = []
 
-            res.on('data', d => {
-                data.push(d)
-            })
+      res.on('data', d => {
+        data.push(d)
+      })
 
-            res.on('end', () => {
-                resolve(
-                    HttpResponse(
-                        statusCode,
-                        headers,
-                        Buffer.concat(data).toString(),
-                    ),
-                )
-            })
-        }).on('error', e => {
-            reject(e)
-        })
+      res.on('end', () => {
+        resolve(
+          HttpResponse(statusCode, headers, Buffer.concat(data).toString()),
+        )
+      })
+    }).on('error', e => {
+      reject(e)
     })
+  })
 }
 
 /**
  * Example of positive promise nest to avoid a call back pyramid
  */
 export function MultipleCalls(): Promise<any> {
-    return httpsGet('https://jsonplaceholder.typicode.com/users')
-        .then(res => {
-            // do sth with result
-            return httpsGet('https://jsonplaceholder.typicode.com/posts')
-        })
-        .then(res => {
-            // do sth with result
-            return httpsGet('https://jsonplaceholder.typicode.com/comments')
-        }).then(res => {
-            // do sth with result
-            return Promise.resolve({})
-        })
+  return httpsGet('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      // do sth with result
+      return httpsGet('https://jsonplaceholder.typicode.com/posts')
+    })
+    .then(res => {
+      // do sth with result
+      return httpsGet('https://jsonplaceholder.typicode.com/comments')
+    })
+    .then(res => {
+      // do sth with result
+      return Promise.resolve({})
+    })
 }
 
 /**
  * Using Async/Await Pattern to execute the multiple call promise
  */
 export async function executeMultipleCalls(): Promise<any> {
-    try {
-        return await MultipleCalls()
-        // ES6+: return Promise.resolve(await MultipleCalls())
-    } catch (e) {
-        console.error(e)
-    }
+  try {
+    return await MultipleCalls()
+    // ES6+: return Promise.resolve(await MultipleCalls())
+  } catch (e) {
+    console.error(e)
+  }
 }
